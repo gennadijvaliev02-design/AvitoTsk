6e054d4c6f5341fabc6b1784efae693e9712f230
gennadijvaliev02-design <gennadijvaliev02@gmail.com>
2026-07-22 07:39:54 +0300

    Add Telegram current-user endpoint

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 window.AVITO_CONFIG = Object.freeze({
   listingsApiUrl: 'https://script.google.com/macros/s/AKfycbzWnDuFpTXtoR0RTYEs2P1R-fJvy9cHBuGtoVRV4KTEoyOCCT-QKBqb-HyeFBu73J4g/exec',
   telegramBotUsername: 'AvitoReg_bot',
-  telegramAuthApiUrl: 'https://n8n.anxietycoach.online/webhook/avito-auth-telegram'
+  telegramAuthApiUrl: 'https://n8n.anxietycoach.online/webhook/avito-auth-telegram',
+  telegramAuthMeApiUrl: 'https://n8n.anxietycoach.online/webhook/avito-auth-me'
 });
