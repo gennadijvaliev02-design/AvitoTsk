f129bd03672119f3689edcbcea89a5a902733c43
gennadijvaliev02-design <gennadijvaliev02@gmail.com>
2026-07-22 07:18:03 +0300

    Add configurable Telegram auth endpoints

---
+++ config.js
@@ -0,0 +1,5 @@
+window.AVITO_CONFIG = Object.freeze({
+  listingsApiUrl: 'https://script.google.com/macros/s/AKfycbzWnDuFpTXtoR0RTYEs2P1R-fJvy9cHBuGtoVRV4KTEoyOCCT-QKBqb-HyeFBu73J4g/exec',
+  telegramBotUsername: 'AvitoReg_bot',
+  telegramAuthApiUrl: 'https://n8n.anxietycoach.online/webhook/avito-auth-telegram'
+});
